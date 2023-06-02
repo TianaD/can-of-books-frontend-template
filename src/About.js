0972fc0f8e45a4aa616d9acc02827879a946ab71
TianaD <tldavis99@yahoo.com>
2023-06-02 11:05:59 -0500

    changed classes to functions

--- src/About.js
+++ src/About.js
@@ -1,11 +1,9 @@
 import { Component } from "react";
 
-class Profile extends Component {
+function Profile() {
 
-  render() {
     /* TODO: render information about the developers */
     return <p>Profile page coming soon</p>
-  }
-};
+  };
 
 export default Profile;
